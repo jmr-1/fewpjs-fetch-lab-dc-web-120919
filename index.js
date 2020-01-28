5a92e53662d4bbf6d8d70213348e00c8bb85daa1
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-28 10:25:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
 
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(jsonFile => renderBooks(jsonFile));
 }
 
 function renderBooks(json) {
@@ -12,5 +15,6 @@ function renderBooks(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
+
 })
